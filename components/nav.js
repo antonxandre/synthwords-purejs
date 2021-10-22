9776e20f81e979822c737c666a396ba1a9f67ea9
Antonio Alexandre <4958308-N0TN4@users.noreply.gitlab.com>
2021-10-22 01:01:42 -0300

    Primeiro componente

---
+++ components/nav.js
@@ -0,0 +1,17 @@
+var elem = document.querySelector('#nav');
+
+
+var html = elem.innerHTML;
+elem.innerHTML = `
+<ul>
+    <li>
+    Ínicio
+    </li>
+    <li>
+    História
+    </li>
+    <li>
+    Mais informações
+    </li>
+</ul>
+`;
