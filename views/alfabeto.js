1ecfc895db121c74523b5c47a26505435bc3ecca
Antonio Alexandre <4958308-N0TN4@users.noreply.gitlab.com>
2021-11-13 10:16:23 -0300

    add style anchor hover on letter in alphabet

--- views/alfabeto.js
+++ views/alfabeto.js
@@ -1,10 +1,6 @@
 const alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-
-
-
-
-alfabetHtmlList  = alfabeto.map(e => `<a href='#letra-${e}'> ${e} </a>`  ).join('');
+alfabetHtmlList  = alfabeto.map(e => `<a class="shadowLetter" href='#letra-${e}'> ${e} </a>`  ).join('');
 
 var elem = document.querySelector('#alfabeto').innerHTML = alfabetHtmlList;
 
