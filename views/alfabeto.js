925ada1adfbee13511a146826ede7ed37d200dd6
Antonio Alexandre <4958308-N0TN4@users.noreply.gitlab.com>
2021-11-13 09:52:02 -0300

    Alfabeto first look

---
+++ views/alfabeto.js
@@ -0,0 +1,10 @@
+const alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+
+
+
+
+alfabetHtmlList  = alfabeto.map(e => `<a href='#letra-${e}'> ${e} </a>`  ).join('');
+
+var elem = document.querySelector('#alfabeto').innerHTML = alfabetHtmlList;
+
