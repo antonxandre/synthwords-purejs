f5612b1f9b148e34dd7853de35eb0b9751dd2fcf
Antonio Alexandre <4958308-N0TN4@users.noreply.gitlab.com>
2021-11-13 12:20:49 -0300

    Possibilidade de pressionar em uma letra e adicionar ele a palavra central

--- views/alfabeto.js
+++ views/alfabeto.js
@@ -1,6 +1,21 @@
-const alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-alfabetHtmlList  = alfabeto.map(e => `<a class="shadowLetter" href='#letra-${e}'> ${e} </a>`  ).join('');
+alfabetHtmlList = alfabeto.map(e => `<a class="shadowLetter" onclick="adicionarLetra('${e}')" href="#letra=${e}"> ${e} </a>`).join('');
 
-var elem = document.querySelector('#alfabeto').innerHTML = alfabetHtmlList;
+const elem = document.querySelector('#alfabeto').innerHTML = alfabetHtmlList;
+const palavraDigitada = document.getElementById('palavra-digitada');
 
+
+
+document.addEventListener('keydown', (event) => {
+    var key = event.key;
+    var code = event.code;
+    if (alfabeto.indexOf(key.toUpperCase()) > -1) {
+        palavraDigitada.innerHTML += key;
+    }
+
+}, false);
+
+let adicionarLetra = function (letra) {
+    palavraDigitada.innerHTML += letra;
+}
