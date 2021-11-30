2b9c54785cd4014a901571f96bb28f06ec94b204
Antonio Alexandre <a.a.a.j@hotmail.com.br>
2021-11-30 20:05:10 -0300

    Refact

--- views/alfabeto.js
+++ views/alfabeto.js
@@ -1,6 +1,8 @@
 const alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-alfabetHtmlList = alfabeto.map(e => `<a class="shadowLetter" onclick="adicionarLetra('${e}')" href="#letra=${e}"> ${e} </a>`).join('');
+let alfabetHtmlList = alfabeto.map(e => `<a class="shadowLetter" onclick="adicionarLetra('${e}')" href="#letra=${e}"> ${e} </a>`).join('');
+
+const alfabetoAtualizado = alfabeto;
 
 const elem = document.querySelector('#alfabeto').innerHTML = alfabetHtmlList;
 const palavraDigitada = document.getElementById('palavra-digitada');
@@ -9,13 +11,26 @@ const palavraDigitada = document.getElementById('palavra-digitada');
 
 document.addEventListener('keydown', (event) => {
     var key = event.key;
-    var code = event.code;
+    
     if (alfabeto.indexOf(key.toUpperCase()) > -1) {
-        palavraDigitada.innerHTML += key;
+        adicionarLetra(key.toUpperCase());
+     
     }
-
 }, false);
 
 let adicionarLetra = function (letra) {
-    palavraDigitada.innerHTML += letra;
+    palavraDigitada.innerHTML += letra; 
+    (function () {
+        for (i = 0; i < alfabeto.length; i++) {
+            let index = alfabeto.indexOf(letra);
+            console.log(index);
+            if (index > -1) {
+               alfabeto.splice(index, 1);
+             
+                let novoAlfabeto = alfabeto.map(e => `<a class="shadowLetter" onclick="adicionarLetra('${e}')" href="#letra=${e}"> ${e} </a>`).join('');
+
+                elem.innerHTML = novoAlfabeto;
+            }
+        }
+    })();
 }
